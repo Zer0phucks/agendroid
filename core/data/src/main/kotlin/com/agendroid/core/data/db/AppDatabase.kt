c683925bcaa8f4cc1aaf037405109dc29a8ff25f
Zer0phucks <10062537@cc.peralta.edu>
2026-03-20 19:28:15 -0700

    feat(data): add ConversationSummaryEntity + ConversationSummaryDao

--- core/data/src/main/kotlin/com/agendroid/core/data/db/AppDatabase.kt
+++ core/data/src/main/kotlin/com/agendroid/core/data/db/AppDatabase.kt
@@ -4,14 +4,17 @@ package com.agendroid.core.data.db
 import androidx.room.Database
 import androidx.room.RoomDatabase
 import com.agendroid.core.data.dao.ChunkDao
+import com.agendroid.core.data.dao.ConversationSummaryDao
 import com.agendroid.core.data.dao.KnowledgeDocumentDao
 import com.agendroid.core.data.entity.ChunkEntity
+import com.agendroid.core.data.entity.ConversationSummaryEntity
 import com.agendroid.core.data.entity.KnowledgeDocumentEntity
 
 @Database(
     entities = [
         ChunkEntity::class,
         KnowledgeDocumentEntity::class,
+        ConversationSummaryEntity::class,
     ],
     version = 1,
     exportSchema = true,
@@ -19,4 +22,5 @@ import com.agendroid.core.data.entity.KnowledgeDocumentEntity
 abstract class AppDatabase : RoomDatabase() {
     abstract fun chunkDao(): ChunkDao
     abstract fun knowledgeDocumentDao(): KnowledgeDocumentDao
+    abstract fun conversationSummaryDao(): ConversationSummaryDao
 }
